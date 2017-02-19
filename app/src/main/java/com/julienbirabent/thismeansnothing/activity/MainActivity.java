90d6f51b62c68355a1006a8b16d8a0f2506b67bc
Julien Birabent <jul.birabent@gmail.com>
2017-02-18 19:15:22 -0500

    Ajout du model AudioTrack pour le stockage de l'information d'une piste audio

--- app/src/main/java/com/julienbirabent/thismeansnothing/activity/MainActivity.java
+++ app/src/main/java/com/julienbirabent/thismeansnothing/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.julienbirabent.thismeansnothing;
+package com.julienbirabent.thismeansnothing.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.julienbirabent.thismeansnothing.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/julienbirabent/thismeansnothing/model/AudioTrack.java
@@ -0,0 +1,8 @@
+package com.julienbirabent.thismeansnothing.model;
+
+/**
+ * Created by Julien on 2017-02-18.
+ */
+
+public class AudioTrack {
+}
